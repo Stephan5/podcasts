136b3525df8e5fc58d41706d9b4be87eb7595b4e
Stephan Blakeslee <Stephan5@users.noreply.github.com>
2025-05-20 12:01:24 +0100

    Update all again with description fixes

--- ct/cmd.sh
+++ ct/cmd.sh
@@ -3,5 +3,5 @@
 ./csv2rss.sh ./ct/feed.csv \
   --repo-dir "ct" \
   --title "Cum Town" \
-  --description "A podcast about having sex with your dad" \
+  --description "A podcast about having sex with your dad." \
   --delimiter $'\x1F'
--- snk-xfm/cmd.sh
+++ snk-xfm/cmd.sh
@@ -3,5 +3,5 @@
 ./csv2rss.sh ./snk-xfm/feed.csv \
   --repo-dir "snk-xfm" \
   --title "SNK XFM" \
-  --description "Simon Pegg, Nick Frost and Karl Pilkington on XFM" \
+  --description "Simon Pegg, Nick Frost and Karl Pilkington on XFM." \
   --delimiter ";"
