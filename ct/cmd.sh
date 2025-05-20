e04da4dce9c8b740e4e0ab9cd4c8a2ab15839ae5
Stephan Blakeslee <Stephan5@users.noreply.github.com>
2025-05-20 11:20:03 +0100

    Update ct

--- ct/cmd.sh
+++ ct/cmd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-./csv2rss.sh ./cum/feed.csv \
-  --repo-dir "cum" \
+./csv2rss.sh ./ct/feed.csv \
+  --repo-dir "ct" \
   --title "Cum Town" \
   --description "A podcast about having sex with your dad" \
   --delimiter $'\x1F'
