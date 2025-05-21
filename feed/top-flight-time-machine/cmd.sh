f9772ddf1466d8df642698b1201e963b7dde9f13
Stephan Blakeslee <Stephan5@users.noreply.github.com>
2025-05-21 11:10:30 +0100

    Add feed for TFTM

---
+++ feed/top-flight-time-machine/cmd.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -euo pipefail
+
+# Get absolute paths
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+base_dir="$(cd "$script_dir/../.." && pwd)"
+
+"$base_dir"/script/csv2rss.sh "$script_dir"/feed.csv \
+  --title "Top Flight Time Machine" \
+  --description "Andy Dawson &amp; Sam Delaney - used to be football, all over the place now. Coins, ghosts, digging, dis, dat..." \
+  --delimiter $'\x1F'
