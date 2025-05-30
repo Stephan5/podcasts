a4379a26b7d45b3dbae342b34410e8f33743208b
Stephan Blakeslee <Stephan5@users.noreply.github.com>
2025-05-31 00:24:43 +0100

    [russell-and-karl] Update title and add image

--- feed/russell-and-karl/cmd.sh
+++ feed/russell-and-karl/cmd.sh
@@ -6,6 +6,6 @@ csv_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 base_dir="$(cd "$csv_dir/../.." && pwd)"
 
 "$base_dir"/script/csv2rss.sh "$csv_dir"/feed.csv \
-  --title "The Russell Brand Show with Karl Pilkington" \
-  --description "A few one-off shows of the Russell Brand Show with Karl Pilkington joining as co-host." \
+  --title "Russell Brand and Karl Pilkington" \
+  --description "A few one-off shows of the Russell Brand Show where Karl Pilkington joins as co-host." \
   --delimiter $'\x1F'
