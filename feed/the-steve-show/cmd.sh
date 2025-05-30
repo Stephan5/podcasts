c546b7bf4b16e3a860c0082b2cc2bd018d163973
Stephan Blakeslee <Stephan5@users.noreply.github.com>
2025-05-30 15:26:24 +0100

    Update Adam and Joe and The Steve Show titles/desc

--- feed/the-steve-show/cmd.sh
+++ feed/the-steve-show/cmd.sh
@@ -6,6 +6,6 @@ csv_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 base_dir="$(cd "$csv_dir/../.." && pwd)"
 
 "$base_dir"/script/csv2rss.sh "$csv_dir"/feed.csv \
-  --title "The Steve Show - BBC 6Music" \
-  --description "Stephen Merchant's Sunday afternoon show on BBC 6Music that ran from 2007 to 2009" \
+  --title "The Steve Show" \
+  --description "Stephen Merchant's Sunday afternoon show on BBC 6Music." \
   --delimiter $'\x1F'
