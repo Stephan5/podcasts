97e01b485f95e4b776ea4a7db208ae7aa1472be5
Stephan Blakeslee <Stephan5@users.noreply.github.com>
2025-05-31 01:29:14 +0100

    [adam-and-joe] Fix author

--- feed/adam-and-joe/cmd.sh
+++ feed/adam-and-joe/cmd.sh
@@ -8,5 +8,5 @@ base_dir="$(cd "$csv_dir/../.." && pwd)"
 "$base_dir"/script/csv2rss.sh "$csv_dir"/feed.csv \
   --title "Adam and Joe" \
   --description "The Adam Buxton and Joe Cornish archive of shows across XFM and 6Music." \
-  --author "XFM &amp; 6Music" \
+  --author "XFM &amp; BBC 6Music" \
   --delimiter $'\x1F'
