a193f8b841f7e84c243efd47a010ef12178626f2
Stephan Blakeslee <Stephan5@users.noreply.github.com>
2025-05-31 11:36:11 +0100

    Add Drifter's Sympathy

---
+++ feed/drifters-sympathy/cmd.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -euo pipefail
+
+# Get absolute paths
+csv_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+base_dir="$(cd "$csv_dir/../.." && pwd)"
+
+"$base_dir"/script/csv2rss.sh "$csv_dir"/feed.csv \
+  --title "Drifter's Sympathy" \
+  --description "Emil Amos charts the birth and development of the classic archetype 'The Outsider', telling disturbing and often humiliating stories about growing up in a small town in the 90’s. Every other episode digs into the archaeology of lesser-known music to illuminate the same themes from a more objective, historical perspective." \
+  --author "Emil Amos" \
+  --delimiter $'\x1F'
