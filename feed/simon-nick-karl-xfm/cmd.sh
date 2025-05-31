36fe37a516ee36f57fc421d097570a2f5c6dae11
Stephan Blakeslee <Stephan5@users.noreply.github.com>
2025-05-31 01:28:54 +0100

    [simon-nick-karl-xfm] Update title

--- feed/simon-nick-karl-xfm/cmd.sh
+++ feed/simon-nick-karl-xfm/cmd.sh
@@ -6,7 +6,7 @@ csv_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 base_dir="$(cd "$csv_dir/../.." && pwd)"
 
 "$base_dir"/script/csv2rss.sh "$csv_dir"/feed.csv \
-  --title "SNK XFM" \
+  --title "Simon, Nick &amp; Karl" \
   --description "Simon Pegg, Nick Frost and Karl Pilkington on XFM." \
   --author "XFM" \
   --delimiter ";"
