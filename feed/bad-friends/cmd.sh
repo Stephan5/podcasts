811584b63dd44bb68d304271ce219590b73314f2
Stephan Blakeslee <Stephan5@users.noreply.github.com>
2025-05-31 02:29:50 +0100

    Add Bad Friends

---
+++ feed/bad-friends/cmd.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -euo pipefail
+
+# Get absolute paths
+csv_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+base_dir="$(cd "$csv_dir/../.." && pwd)"
+
+"$base_dir"/script/csv2rss.sh "$csv_dir"/feed.csv \
+  --title "Bad Friends" \
+  --description "Andrew Santino and Bobby Lee present BAD FRIENDS" \
+  --author "Andrew Santino &amp; Bobby Lee" \
+  --delimiter $'\x1F'
