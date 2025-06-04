4b9ffe5d2697d588574f495085a5cfcd1fd6652a
Stephan Blakeslee <Stephan5@users.noreply.github.com>
2025-06-04 16:31:01 +0100

    [feed] Add alan partridge

---
+++ feed/alan-partridge/cmd.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -euo pipefail
+
+# Get absolute paths
+csv_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+base_dir="$(cd "$csv_dir/../.." && pwd)"
+
+"$base_dir"/script/csv2rss.sh "$csv_dir"/feed.csv \
+  --title "Alan Partridge" \
+  --description "A collection of audiobooks and podcasts from Alan Partridge." \
+  --author "Steve Coogan" \
+  --delimiter ";"
